0e4f5e15dc3a06da0abefdd2f35ed38f95f3ebc1
JustKilli <justkilli@outlook.de>
2023-08-12 19:15:22 +0200

    Base ConfigHandler implementation

--- src/main/java/net/justkilli/base/Main.java
+++ src/main/java/net/justkilli/base/Main.java
@@ -1,5 +1,7 @@
 package net.justkilli.base;
 
+import net.justkilli.config.ConfigHandler;
+import net.justkilli.config.IConfigHandler;
 import net.justkilli.logging.ILogger;
 import net.justkilli.logging.LogLevel;
 import net.justkilli.logging.loggers.LoggerBuilder;
@@ -7,10 +9,13 @@ import net.justkilli.logging.output.ConsolePrinter;
 import net.justkilli.logging.output.DialogPrinter;
 import net.justkilli.logging.output.FilePrinter;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
-        ILogger logger = new LoggerBuilder("Main")
+        //Logger Test
+/*        ILogger logger = new LoggerBuilder("Main")
                 .addOutputPrinter(new ConsolePrinter())
                 .addOutputPrinter(new FilePrinter())
                 .addOutputPrinter(new DialogPrinter())
@@ -18,6 +23,9 @@ public class Main {
         logger.log(LogLevel.INFO, "Hello world!");
         logger.log(LogLevel.WARN, "Hello world!");
         logger.log(LogLevel.ERROR, "Hello world!");
-        logger.log(LogLevel.DEBUG, "Hello world!");
+        logger.log(LogLevel.DEBUG, "Hello world!");*/
+
+        IConfigHandler configHandler = new ConfigHandler("config.yml");
+
     }
 }
---
+++ src/main/java/net/justkilli/config/ConfigHandler.java
@@ -0,0 +1,51 @@
+package net.justkilli.config;
+
+import org.simpleyaml.configuration.file.YamlFile;
+
+import java.io.IOException;
+
+public class ConfigHandler implements IConfigHandler {
+
+    private YamlFile config;
+
+    public ConfigHandler(String path) throws IOException {
+        config = init(path);
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    @Override
+    public YamlFile init(String path) throws IOException {
+        config = new YamlFile(path);
+        config.options().copyDefaults(true);
+        if(config.exists()) return load();
+        else return create();
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    @Override
+    public YamlFile create() throws IOException {
+        config.createNewFile();
+        return config;
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    @Override
+    public YamlFile load() throws IOException {
+        config.load();
+        return config;
+    }
+
+    /**
+     * {@inheritDoc}
+     * */
+    @Override
+    public YamlFile getConfig() {
+        return config;
+    }
+}
