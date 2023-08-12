83a82582b6682140c02d034c1b70f61f49187b2a
JustKilli <justkilli@outlook.de>
2023-08-12 22:06:19 +0200

    ConfigValue changed, to Support ConfigParameter

--- src/main/java/net/justkilli/config/values/ConfigValue.java
+++ src/main/java/net/justkilli/config/values/ConfigValue.java
@@ -1,15 +1,24 @@
 package net.justkilli.config.values;
 
-public record ConfigValue<T>(String path, T value) {
+public record ConfigValue<T>(String path, T value, ConfigParameter... parameters) {
 
-    public static final ConfigValue<String> STRING = new ConfigValue<String>("string", "Test String Value");
+    public static final ConfigValue<String> STRING = new ConfigValue<String>("string", String.format("Test String %s Value", ConfigParameter.TEST_PARAMETER), ConfigParameter.TEST_PARAMETER);
     public static final ConfigValue<Boolean> BOOLEAN = new ConfigValue<Boolean>("boolean", true);
     public static final ConfigValue<Integer> INTEGER = new ConfigValue<Integer>("integer", 1);
     public static final ConfigValue<Double> DOUBLE = new ConfigValue<Double>("double", 1.0);
 
     @Override
     public String toString() {
-        return String.valueOf(value());
+        return replaceParameter();
+    }
+
+    public String replaceParameter() {
+        if(!(value instanceof String)) return String.valueOf(value);
+        String value = (String) this.value;
+        for(ConfigParameter parameter : parameters) {
+            value = value.replace(parameter.toString(), parameter.value());
+        }
+        return value;
     }
 
 }
