f27fd8278464610d1da73f037a7f312d95897e5a
JustKilli <justkilli@outlook.de>
2023-08-13 05:05:30 +0200

    clean up

--- src/main/java/net/justkilli/killisessentials/base/Main.java
+++ src/main/java/net/justkilli/killisessentials/base/Main.java
@@ -1,52 +1,27 @@
 package net.justkilli.killisessentials.base;
 
-import net.justkilli.config.ConfigUtils;
-import net.justkilli.config.handler.YAMLConfigHandler;
-import net.justkilli.config.handler.IConfigHandler;
-import net.justkilli.config.values.ConfigParameter;
-import net.justkilli.config.values.ConfigValue;
+import net.justkilli.killisessentials.config.handler.IConfigHandler;
+import net.justkilli.killisessentials.config.handler.YAMLConfigHandler;
+import net.justkilli.killisessentials.database.DBAccessLayer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
 
 public class Main extends JavaPlugin {
 
-        // Plugin startup logic
-        getLogger().info("Hello world!");
+        @Override
+        public void onEnable() {
+            getLogger().info("Killi's Essentials has been enabled!");
+            try {
+                IConfigHandler configHandler = new YAMLConfigHandler("config.yml");
+                DBAccessLayer dbAccessLayer = new DBAccessLayer(configHandler);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-    @Override
-    public void onDisable() {
-        // Plugin startup logic
-        getLogger().info("Hello world!");
-    }
-
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-        //Logger Test
-/*        ILogger logger = new LoggerBuilder("Main")
-                .addOutputPrinter(new ConsolePrinter())
-                .addOutputPrinter(new FilePrinter())
-                .addOutputPrinter(new DialogPrinter())
-                .build();
-        logger.log(LogLevel.INFO, "Hello world!");
-        logger.log(LogLevel.WARN, "Hello world!");
-        logger.log(LogLevel.ERROR, "Hello world!");
-        logger.log(LogLevel.DEBUG, "Hello world!");*/
-        IConfigHandler configHandler = new YAMLConfigHandler("config.yml");
-        configHandler.addDefaultValue(ConfigValue.STRING);
-        configHandler.addDefaultValue(ConfigValue.INTEGER);
-        configHandler.addDefaultValue(ConfigValue.BOOLEAN);
-        configHandler.addDefaultValue(ConfigValue.DOUBLE);
-
-        ConfigValue<Boolean> booleanSetConfigValue = new ConfigValue<>(ConfigValue.BOOLEAN.path(), false);
-        configHandler.setValue(booleanSetConfigValue);
-
-        String str = configHandler.getString(ConfigValue.STRING).toString();
-        Boolean bool = configHandler.getBoolean(ConfigValue.BOOLEAN).value();
-        System.out.println(str);
-        System.out.println(bool);
-        String str2 = configHandler.getString(ConfigUtils.createConfigValue(ConfigValue.STRING, ConfigParameter.TEST_PARAMETER.copy("Test String"))).toString();
-        System.out.println(str2);
-
-    }
+        @Override
+        public void onDisable() {
+            getLogger().info("Killi's Essentials has been disabled!");
+        }
 }
