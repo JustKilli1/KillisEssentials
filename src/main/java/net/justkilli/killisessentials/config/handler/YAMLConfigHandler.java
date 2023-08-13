3ca42f61bf6da354e827a5ef60d4efafa540cc8c
JustKilli <justkilli@outlook.de>
2023-08-13 22:59:24 +0200

    Reloadable interface implemented, Exception handling added

--- src/main/java/net/justkilli/killisessentials/config/handler/YAMLConfigHandler.java
+++ src/main/java/net/justkilli/killisessentials/config/handler/YAMLConfigHandler.java
@@ -3,6 +3,10 @@ package net.justkilli.killisessentials.config.handler;
 import net.justkilli.killisessentials.config.ConfigUtils;
 import net.justkilli.killisessentials.config.values.ConfigParameter;
 import net.justkilli.killisessentials.config.values.ConfigValue;
+import net.justkilli.killisessentials.logging.ILogger;
+import net.justkilli.killisessentials.logging.LogLevel;
+import net.justkilli.killisessentials.logging.loggers.LoggerBuilder;
+import net.justkilli.killisessentials.logging.output.ConsolePrinter;
 import org.simpleyaml.configuration.file.YamlFile;
 
 import java.io.IOException;
@@ -12,9 +16,12 @@ import java.util.stream.Stream;
 
 public class YAMLConfigHandler implements IConfigHandler {
 
+    private static final ILogger CONFIG_LOGGER = new LoggerBuilder("Killi's-Essentials-Configs")
+            .addOutputPrinter(new ConsolePrinter())
+            .build();
     private YamlFile config;
 
-    public YAMLConfigHandler(String path) throws IOException {
+    public YAMLConfigHandler(String path) {
         config = init(path);
     }
 
@@ -22,9 +29,10 @@ public class YAMLConfigHandler implements IConfigHandler {
      * {@inheritDoc}
      * */
     @Override
-    public YamlFile init(String path) throws IOException {
+    public YamlFile init(String path) {
         config = new YamlFile(path);
         config.options().copyDefaults(true);
+
         if(config.exists()) return load();
         else return create();
     }
@@ -33,26 +41,40 @@ public class YAMLConfigHandler implements IConfigHandler {
      * {@inheritDoc}
      * */
     @Override
-    public YamlFile create() throws IOException {
-        config.createNewFile();
-        return config;
+    public YamlFile create() {
+        try {
+            config.createNewFile();
+            return config;
+        } catch (IOException ex) {
+            CONFIG_LOGGER.log(LogLevel.ERROR, String.format("Config File with path %s could not be created", config.getFilePath()), ex);
+            return null;
+        }
     }
 
     /**
      * {@inheritDoc}
      * */
     @Override
-    public YamlFile load() throws IOException {
-        config.load();
-        return config;
+    public YamlFile load() {
+        try {
+            config.load();
+            return config;
+        } catch(Exception ex) {
+            CONFIG_LOGGER.log(LogLevel.ERROR, String.format("Config File with path %s could not be loaded", config.getFilePath()), ex);
+            return null;
+        }
     }
 
     /**
      * {@inheritDoc}
      * */
     @Override
-    public void save() throws IOException {
-        config.save();
+    public void save() {
+        try {
+            config.save();
+        } catch (IOException ex) {
+            CONFIG_LOGGER.log(LogLevel.ERROR, String.format("Config File with path %s could not be saved", config.getFilePath()), ex);
+        }
     }
 
     /**
@@ -67,7 +89,7 @@ public class YAMLConfigHandler implements IConfigHandler {
      * {@inheritDoc}
      * */
     @Override
-    public <T> void addDefaultValue(ConfigValue<T> value) throws IOException {
+    public <T> void addDefaultValue(ConfigValue<T> value) {
         config.addDefault(value.path(), value.value());
         setComments(value);
         save();
@@ -77,7 +99,7 @@ public class YAMLConfigHandler implements IConfigHandler {
      * {@inheritDoc}
      * */
     @Override
-    public <T> void setComments(ConfigValue<T> value) throws IOException {
+    public <T> void setComments(ConfigValue<T> value) {
         if(value.parameters().length == 0) return;
     String comments = Stream.concat(
                                     Stream.of("Parameter Options:"), Arrays.stream(value.parameters())
@@ -91,7 +113,7 @@ public class YAMLConfigHandler implements IConfigHandler {
      * {@inheritDoc}
      * */
     @Override
-    public <T> void setValue(ConfigValue<T> value) throws IOException {
+    public <T> void setValue(ConfigValue<T> value) {
         config.set(value.path(), value.toString());
         save();
     }
@@ -130,5 +152,11 @@ public class YAMLConfigHandler implements IConfigHandler {
     public ConfigValue<Double> getDouble(ConfigValue<Double> value) {
         Double configValue = config.getDouble(value.path());
         return ConfigUtils.createConfigValue(value, configValue);
+    }
+
+    @Override
+    public boolean reload() {
+        load();
+        return false;
     }
 }
