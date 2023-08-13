64cfffbb16934dc693a53f1d3044b04126f9564c
JustKilli <justkilli@outlook.de>
2023-08-13 05:03:22 +0200

    Audience messenger added

--- src/main/java/net/justkilli/killisessentials/Messenger.java
+++ src/main/java/net/justkilli/killisessentials/Messenger.java
@@ -1,6 +1,7 @@
 package net.justkilli.killisessentials;
 
 import net.justkilli.killisessentials.config.values.ConfigValue;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
@@ -13,9 +14,21 @@ public class Messenger {
         return miniMessageComponent;
     }
 
+    public static Component sendMessage(Audience audience, String message) {
+        Component miniMessageComponent = MiniMessage.miniMessage().deserialize(message);
+        audience.sendMessage(miniMessageComponent);
+        return miniMessageComponent;
+    }
+
     public static <T> Component sendMessage(Player player, ConfigValue<T> message) {
         Component miniMessageComponent = MiniMessage.miniMessage().deserialize(message.toString());
         player.sendMessage(miniMessageComponent);
+        return miniMessageComponent;
+    }
+
+    public static <T> Component sendMessage(Audience audience, ConfigValue<T> message) {
+        Component miniMessageComponent = MiniMessage.miniMessage().deserialize(message.toString());
+        audience.sendMessage(miniMessageComponent);
         return miniMessageComponent;
     }
 
